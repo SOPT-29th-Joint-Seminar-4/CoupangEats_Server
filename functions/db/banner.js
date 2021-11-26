c2c7a8af5885db3e55bd72f873518a5d4f550ccf
Youjung Heo <hujumee27@gmail.com>
2021-11-26 19:14:39 +0900

    [add] 배너 조회 구현

---
+++ functions/db/banner.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+
+const getBanner = async (client, osId) => {
+  const { rows } = await client.query(
+    `
+    SELECT id, image FROM banner b
+    WHERE id = $1
+    `,
+    [osId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { getBanner };
