6c78a29b0b0458974afc6918f4bd04f8e2fa687a
kmebin <blowhuiee@gmail.com>
2021-11-26 16:33:50 +0900

    [add] reponseMessage 추가

---
+++ functions/constants/responseMessage.js
@@ -0,0 +1,11 @@
+module.exports = {
+  NULL_VALUE: '필요한 값이 없습니다',
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+
+  // 메뉴
+  READ_MENU_SUCCESS: '메뉴 조회 성공',
+  NO_SHOP: '존재하지 않는 가게입니다.',
+
+  // 서버 내 오류
+  INTERNAL_SERVER_ERROR: '서버 내 오류',
+};
